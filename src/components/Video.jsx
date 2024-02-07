04ca078160decfe467d0043bd8f33c0fefa8ffd8
ricardo <ohcanikissu@gmail.com>
2024-02-07 09:52:06 +0800

    wip(components):等高

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,14 @@
 const Video = ({id, cover, note, title}) => {
     return <div className={`item
             w-[calc(12%-18px)]
+            max-md:w-[calc(25%-18px)]
             max-lg:w-[calc(20%-18px)]
             max-2xl:w-[calc(16%-18px)]
             max-3xl:w-[calc(14%-18px)]
     `}>
-        <div className={`cover relative`}>
+        <div className={`cover relative pt-[140%]`}>
             <div className={`cover`}>
-                <img src={cover} alt={title} className={'w-full h-full object-cover rounded-[8px]'}/>
+                <img src={cover} alt={title} className={'absolute top-0 left-0 w-full h-full object-cover rounded-[8px]'}/>
             </div>
             <div className={`note absolute left-1/2 translate-x-1/2 bottom-3 text-white one-lines-ellipsis`}>
                 {note}
