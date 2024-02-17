4b828826ea8e351805634640a920320eb8b3b2ca
ricardo <ohcanikissu@gmail.com>
2024-02-17 21:17:31 +0800

    fix(header):back

--- src/pages/base/header/Header.jsx
+++ src/pages/base/header/Header.jsx
@@ -1,17 +1,11 @@
 import SearchBar from "./cpn/SearchBar.jsx";
 import Logo from "./cpn/Logo.jsx";
-import {useNavigate} from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const onBackHome = () => {
-        navigate('/');
-    }
 
     return <div className={`fixed left-0 top-0 right-0 z-[11] margin-y-auto bg-[rgba(247,248,249,0.97)] h-[64px]
         shadow-md flex items-center justify-center px-[20px] cursor-pointer
     `}
-         onClick={onBackHome}
     >
         <Logo/>
         <SearchBar/>
--- src/pages/base/header/cpn/Logo.jsx
+++ src/pages/base/header/cpn/Logo.jsx
@@ -1,7 +1,16 @@
+import {useNavigate} from "react-router-dom";
+
 const logoSrc = "https://oss-cdn.meowa.cn/mxtheme/images/logos.png"
 
 const Logo = () => {
-    return <div className={'w-[200px] mr-[10px]'}>
+    const navigate = useNavigate();
+    const onBackHome = () => {
+        navigate('/');
+    }
+
+    return <div className={'w-[200px] mr-[10px]'}
+                onClick={onBackHome}
+    >
         <img width={'100%'} height={'100%'} src={logoSrc} alt=""/>
     </div>
 }
