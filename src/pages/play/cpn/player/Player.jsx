052a64470a4bc2ef390288bb40eceb6ec58c643e
ricardo <ohcanikissu@gmail.com>
2024-02-17 21:15:48 +0800

    fix(play):播放

--- src/pages/play/cpn/player/Player.jsx
+++ src/pages/play/cpn/player/Player.jsx
@@ -3,7 +3,7 @@ import Loading from "../../../base/loading/Loading.jsx";
 import ErrorBlock from "../../../base/error-block/ErrorBlock.jsx";
 import {useParams} from "react-router-dom";
 import {useEffect} from "react";
-import {useSelector} from "react-redux";
+import {getVideo} from "@/api/detail.js";
 
 const Player = () => {
     const {id,sid,nid} = useParams();
@@ -12,12 +12,17 @@ const Player = () => {
         playLine: sid,
         episodeId: nid
     });
+    const {data:video,isLoading:videoLoading} = getVideo({id});
 
-    const video = useSelector(state => state.play.playVideo);
+    // const video = useSelector(state => state.play.playVideo);
 
     useEffect(()=>{
         document.title = `${video?.title} - 第${nid}集`;
     },[video])
+
+    if (videoLoading) {
+        return <Loading/>
+    }
 
     if (isLoading) return <Loading />;
     if (error) return <ErrorBlock />;
@@ -27,7 +32,7 @@ const Player = () => {
 
     return <div className={'w-full h-[50vh] min-h-[300px] min-w-[360px] flex-grow'}>
         <iframe border="0" src={
-            `https://player.mcue.cc/yinhua/?url=${data?.url}&next=${!data.url_next ? '' : window.location.protocol + '//' + window.location.host + data?.link_next}&title=${video.title}`
+            `https://player.mcue.cc/yinhua/?url=${data?.url}&next=${!data.url_next ? '' : window.location.protocol + '//' + window.location.host + data?.link_next}&title=${video?.title}`
         }
                 width="100%"
                 height="100%"
--- src/pages/play/cpn/side/main/Content.jsx
+++ src/pages/play/cpn/side/main/Content.jsx
@@ -14,7 +14,9 @@ const Content = () => {
 
     return <div className={`flex p-[10px] gap-1 flex-wrap bg-white rounded-[8px]`}>
         {data?.episodeList?.map((item, index) => {
-            return <div className={`episode border border-solid border-[#e9ecef] py-[2px] px-[8px] rounded-[8px] cursor-pointer
+            return <div
+                key={index}
+                className={`episode border border-solid border-[#e9ecef] py-[2px] px-[8px] rounded-[8px] cursor-pointer
                 text-center text-l text-[#333] hover:bg-[#f5f5f5] hover:text-[#333] transition-all duration-300 ease-in-out
                 w-[79px] h-[30px] ${item.playLine === activePlayLine ? '' : 'hidden'}
             `}
