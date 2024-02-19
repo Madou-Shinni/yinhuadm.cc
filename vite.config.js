8ebfe1579b75c5a1805044c30848d6ffa6b2f2c1
ricardo <ohcanikissu@gmail.com>
2024-02-19 20:57:57 +0800

    docs:README.md

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
     proxy: {
       // 代理所有`/api`的请求
       '/api': {
-        // target: 'http://185.242.234.97:10001',
         target: 'http://127.0.0.1:8080',
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
